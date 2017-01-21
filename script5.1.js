47233b13daeb1754ec5039a4cd6ab095026f4f90
Konika13 <konika.agarwal1994@gmail.com>
2017-01-22 00:36:26 +0530

    Add files via upload

---
+++ script5.1.js
@@ -0,0 +1,9 @@
+var name ="Konika Agarwal";
+var age =22;
+var DOB = "13/09/1994";
+var place="Bilaspur";
+
+console.log("Name:"+name)
+console.log("Age:"+age )
+console.log("DOB:"+DOB)
+console.log("Place of birth:"+place)
